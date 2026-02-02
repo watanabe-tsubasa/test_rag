b443c5cfdfefe70984082f94195b3f7e57541dc3
Tsubasa Watanabe <t.watanabe423@gmail.com>
2026-02-02 02:11:59 +0000

    db:migrate

--- test-rag-app/drizzle/0000_fine_blue_blade.sql
+++ test-rag-app/drizzle/0000_fine_blue_blade.sql
@@ -1,6 +1,6 @@
 CREATE TABLE "documents" (
 	"id" uuid PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
 	"content" text NOT NULL,
-	"embedding" text,
+	"embedding" vector(1536) NOT NULL,
 	"created_at" timestamp DEFAULT now()
 );
--- test-rag-app/drizzle/0001_early_rick_jones.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE "documents" ALTER COLUMN "embedding" SET DATA TYPE vector(1536);--> statement-breakpoint
-ALTER TABLE "documents" ALTER COLUMN "embedding" SET NOT NULL;
